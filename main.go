a45ded52141e68c571985963f7f3e21d376a497d
Zane Wilson <wilsonzanea@gmail.com>
2023-09-26 13:05:21 -0400

    booleans and conditionals

--- main.go
+++ main.go
@@ -7,11 +7,44 @@ import (
 )
 
 func main() {
+	booleansAndConditionals()
 	// loops()
 	// standardLib()
 	// arraysAndSlices()
 	// stringsAndNums()
 	// fmtStuff()
+}
+
+func booleansAndConditionals() { // REF -
+	age := 35
+
+	// fmt.Println(age <= 50)
+	// fmt.Println(age >= 50)
+	// fmt.Println(age == 45)
+	// fmt.Println(age != 50)
+
+	if age < 30 {
+		fmt.Println("Age is less than 30")
+	} else if age < 40 {
+		fmt.Println("Age is less than 40")
+	} else {
+		fmt.Println("Age is greater than or equal to 40")
+	}
+
+	names := []string{"Zane", "Lilly", "Theodore", "Penelope", "Koopa"}
+
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("Continuing at position", index)
+			continue // skips the rest of the code in the loop and goes to the next iteration
+		}
+		if index > 2 {
+			fmt.Println("Breaking at position", index)
+			break // breaks out of the loop
+		}
+
+		fmt.Printf("The name at index %v is %v\n", index, value)
+	}
 }
 
 func loops() { // REF -
@@ -72,7 +105,6 @@ func standardLib() { // REF - https://pkg.go.dev/std
 }
 
 func arraysAndSlices() { // REF - https://go.dev/ref/spec#Array_types
-
 	// var ages [3]int = [3]int{20, 25, 30}
 	var ages = [3]int{20, 25, 30} // type inference
 
@@ -104,7 +136,6 @@ func arraysAndSlices() { // REF - https://go.dev/ref/spec#Array_types
 }
 	
 func stringsAndNums() { // REF - https://go.dev/ref/spec#Numeric_types
-
 	// strings
 	var nameOne string = "Zane"
 	var nameTwo = "2"
